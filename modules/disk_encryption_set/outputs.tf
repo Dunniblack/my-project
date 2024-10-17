fccd20874aa93e977b8fa5bfe8393361db5b6e42
Emmanuel (Demilade) Adebayo <adebayodemilade7@gmail.com>
2024-10-17 22:01:37 +0100

    Update outputs.tf

--- modules/disk_encryption_set/outputs.tf
+++ modules/disk_encryption_set/outputs.tf
@@ -1,9 +1,9 @@
 output "disk_encryption_set_id" {
   description = "The ID of the Disk Encryption Set"
-  value       = azurerm_disk_encryption_set.des.id
+  value       = azurerm_disk_encryption_set.DEJOMSIL5DSKES.id
 }
 
 output "disk_encryption_set_identity" {
   description = "The identity of the Disk Encryption Set"
-  value       = azurerm_disk_encryption_set.des.identity[0]
+  value       = azurerm_disk_encryption_set.DEJOMSIL5DSKES.identity[0]
 }
