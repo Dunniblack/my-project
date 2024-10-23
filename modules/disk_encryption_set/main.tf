4029ac9a0939a4238ece85e4b0a6e097fc067fda
Emmanuel (Demilade) Adebayo <adebayodemilade7@gmail.com>
2024-10-23 17:01:20 +0100

    Update main.tf

--- modules/disk_encryption_set/main.tf
+++ modules/disk_encryption_set/main.tf
@@ -9,9 +9,48 @@ resource "azurerm_disk_encryption_set" "disk_encryption_set" {
   }
 }
 
+resource "azurerm_key_vault_access_policy" "disk-encryption-access" {
+  key_vault_id = var.key_vault_id
+
+  tenant_id = azurerm_disk_encryption_set.disk_encryption_set.identity[0].tenant_id
+  object_id = azurerm_disk_encryption_set.disk_encryption_set.identity[0].principal_id
+
+  key_permissions = [
+    "Create",
+    "Delete",
+    "Get",
+    "Purge",
+    "Recover",
+    "Update",
+    "List",
+    "Decrypt",
+    "Sign",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "user-access" {
+  key_vault_id = var.key_vault_id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Create",
+    "Delete",
+    "Get",
+    "Purge",
+    "Recover",
+    "Update",
+    "List",
+    "Decrypt",
+    "Sign",
+    "GetRotationPolicy",
+  ]
+}
+
 # Grant the Disk Encryption Set access to the Key Vault
 resource "azurerm_role_assignment" "DEJOMIL5DSKESKVCU" {
   scope                = var.key_vault_id
   role_definition_name = "Key Vault Crypto Service Encryption User"
-  principal_id         = azurerm_disk_encryption_set.DEJOMSIL5DSKES.identity[0].principal_id
+  principal_id         = azurerm_disk_encryption_set.disk_encryption_set.identity[0].principal_id
 }
