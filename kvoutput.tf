761fc77d40b0f9ec159880e7feb10cddbb16247b
Dunniblack <99226935+Dunniblack@users.noreply.github.com>
2024-10-24 16:31:02 -0500

    Add files via upload

---
+++ kvoutput.tf
@@ -0,0 +1,17 @@
+# Output the Key Vault ID
+output "key_vault_id" {
+  description = "The ID of the NIST 800-53 compliant Azure Key Vault."
+  value       = azurerm_key_vault.nist_compliant_kv.id
+}
+
+# Output the Key Vault URI
+output "key_vault_uri" {
+  description = "The URI of the Azure Key Vault used for accessing secrets, keys, and certificates."
+  value       = azurerm_key_vault.nist_compliant_kv.vault_uri
+}
+
+# Output the Key Vault Tenant ID (Optional)
+output "tenant_id" {
+  description = "The Azure Active Directory Tenant ID associated with the Key Vault."
+  value       = azurerm_key_vault.nist_compliant_kv.tenant_id
+}
