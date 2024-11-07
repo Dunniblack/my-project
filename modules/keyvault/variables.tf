e8ff4c611b54469f1b12ff56294c6c3cd1b354bb
Dunniblack <99226935+Dunniblack@users.noreply.github.com>
2024-11-07 13:43:36 -0500

    Update variables.tf

--- modules/keyvault/variables.tf
+++ modules/keyvault/variables.tf
@@ -1,31 +1,17 @@
-# Variables for Azure resources
-
-variable "environment_name" {
-  description = "The name of the Environment"
+# modules/keyvault/variables.tf
+variable "key_vault_name" {
   type        = string
+  description = "The name of the Key Vault."
 }
 
-variable "stage" {
-  description = "The stage of the Environment (dev, test, prod)"
+variable "resource_group_name" {
   type        = string
-}
-
-variable "project" {
-  description = "The Project Name"
-  type        = string
-  default     = "JOMS"
-}
-
-variable "functional_area" {
-  description = "The Functional Area of the environment (AFMC)"
-  type        = string
-  default     = "AFMC"
+  description = "The resource group in which to create the Key Vault."
 }
 
 variable "location" {
-  description = "The Azure location where resources will be created."
   type        = string
-  default     = "USGov Virginia"
+  description = "The location where the Key Vault will be created."
 }
 
 variable "tenant_id" {
@@ -33,32 +19,22 @@ variable "tenant_id" {
   description = "Azure Active Directory tenant ID."
 }
 
-variable "subscription_id" {
-  description = "The Subscription ID for Azure."
+variable "log_analytics_workspace_id" {
   type        = string
+  description = "Log Analytics Workspace ID for diagnostics and auditing."
 }
 
-variable "resource_group_name" {
-  description = "The name of the Resource Group."
+variable "subnet_id" {
   type        = string
+  description = "The ID of the subnet for the private endpoint."
 }
 
 variable "vnet_name" {
   type        = string
-  description = "The Virtual Network Name."
+  description = "The name of the existing VNET to use"
 }
 
 variable "subnet_name" {
   type        = string
-  description = "The Subnet Name."
-}
-
-variable "core_resource_group_name" {
-  type        = string
-  description = "The name of the Core Resource Group."
-}
-
-variable "log_analytics_workspace_name" {
-  type        = string
-  description = "The Log Analytics Workspace Name."
+  description = "The name of the subnet within the VNET to use"
 }
