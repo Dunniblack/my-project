9a0044d4691f1f253ae64722b08c4bc51f561e21
Dunniblack <99226935+Dunniblack@users.noreply.github.com>
2024-11-07 13:42:19 -0500

    Update outputs.tf

--- modules/keyvault/outputs.tf
+++ modules/keyvault/outputs.tf
@@ -1,22 +1,15 @@
-# Output the Key Vault ID
-output "key_vault_id" {
-  description = "The ID of the NIST 800-53 compliant Azure Key Vault."
-  value       = azurerm_key_vault.nist_compliant_kv.id
+output "environment_name" {
+  value = var.environment_name
 }
 
-# Output the Key Vault URI
-output "key_vault_uri" {
-  description = "The URI of the Azure Key Vault used for accessing secrets, keys, and certificates."
-  value       = azurerm_key_vault.nist_compliant_kv.vault_uri
+output "project" {
+  value = var.project
 }
 
-# Output the Key Vault Tenant ID (Optional)
-output "tenant_id" {
-  description = "The Azure Active Directory Tenant ID associated with the Key Vault."
-  value       = azurerm_key_vault.nist_compliant_kv.tenant_id
+output "c1_project_name" {
+  value = module.name-map.c1_project_name
 }
 
-output "key_vault_key_id" {
-  description = "The ID of the Key Vault Key used for disk encryption"
-  value       = azurerm_key_vault_key.disk_encryption_key.id
+output "c1_base_name" {
+  value = module.name-map.c1_base_name
 }
